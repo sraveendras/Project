f0b8e2f7fe7056453776c031302edfc0e917ff4e
RC <RC@Ravi>
2019-01-03 22:21:03 +0530

    First Commit

---
+++ ProJectGit/src/MyPackage/LoginPage.java
@@ -0,0 +1,19 @@
+package MyPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://google.com");
+
+	}
+
+}
